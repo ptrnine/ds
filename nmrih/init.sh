f3e176b20d583d9660b26742f0703343fdb48480
Slava Aseev <sockskilling@gmail.com>
2023-11-05 05:54:02 +0300

    Set fastdl ip from .env

--- nmrih/init.sh
+++ nmrih/init.sh
@@ -5,3 +5,5 @@ printf "" > nmrih/cfg/mapcycle.txt
 for map in $(find nmrih/maps/ -name "nmo*.bsp" -printf "%f\n"); do
     echo "${map%.*}" >> nmrih/cfg/mapcycle.txt
 done
+
+echo "sv_downloadurl http://${NMRIH_FASTDL}:6996/" >> $HOME/nmrih/cfg/server.cfg
